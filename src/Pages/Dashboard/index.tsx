23d455297c69023b21a82ef2ba4790f9b6a11b84
danielvfo <danielvfo@gmail.com>
2020-07-11 16:00:52 -0300

    Add local storage support

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 import { Title, Form, Repositories, Error } from './styles';
 import logo from '../../assets/logo.svg';
@@ -16,7 +16,22 @@ interface Repository {
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
   const [inputError, setInputError] = useState('');
-  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const localStorageRepositories = localStorage.getItem(
+      '@github-explorer:repositories',
+    );
+    if (localStorageRepositories) {
+      return JSON.parse(localStorageRepositories);
+    }
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@github-explorer:repositories',
+      JSON.stringify(repositories),
+    );
+  }, [repositories]);
 
   const handleAddRepositories = async (
     event: FormEvent<HTMLFormElement>,
